b0e936e1351cd0714099f1cfe6cb312c3df84098
Krish <krishmali1125@gmail.com>
2026-05-23 15:39:31 +0530

    changes

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.database.database import engine
 from app.database import models
 from app.routes.hostel_routes import router as hostel_router
@@ -7,7 +8,18 @@ from app.routes.auth_routes import router as auth_router
 app = FastAPI(
     title="Hostel Vacancy API"
 )
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(hostel_router)
